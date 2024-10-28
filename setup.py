99a57cef86d8ae5eb864bfe46cfb393255fd8496
Blake Wilson <blake.wilson@quantinuum.com>
2024-10-28 17:57:14 +0000

    Cleaning up. Removed setup.py

--- setup.py
+++ setup.py
@@ -2,39 +2,4 @@ import os
 from setuptools import setup, find_packages
 
 
-def read(filename):
-    filepath = os.path.join(os.path.dirname(__file__), filename)
-    file = open(filepath, "r")
-    return file.read()
-
-
-setup(
-    name="polytensor",
-    version="0.0.6",
-    author="Blake Wilson",
-    author_email="wilso692@purdue.edu",
-    description="",
-    long_description=read("README.rst"),
-    long_description_content_type="text/markdown",
-    license="MIT License",
-    keywords=[],
-    url="",
-    packages=find_packages(),
-    scripts=[],
-    install_requires=[
-        "numpy>=1.25.0",
-        "torch>=2.0.0",
-    ],
-    include_package_data=True,
-    zip_safe=False,
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    ],
-)
+setup()
